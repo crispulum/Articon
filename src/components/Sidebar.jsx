a8e75c6f5c3b73d2878c4426cbc85c71ebdb04cd
Louis Kuczykowski <louisk@Louiss-Mac-mini.local>
2023-10-12 21:33:27 -0400

    feat(headerAndSidebar)created the header/navbar and sidebars for the main page. Going to try to integrate ImageGallery

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,77 @@
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
+
+
+const Sidebar = () => {
+    const [username, setUsername] = useState("Louis");
+    const [clickedEmotion, setClickedEmotion] = useState("");
+    const [searchValue, setSearchValue] = useState('');
+
+    const handleInputChange = (event) => {
+        setSearchValue(event.target.value);
+    };
+
+
+    const onClickEmotionsBtn = (emotion) => {
+        
+
+    }
+
+    
+    return(
+        <>  
+            <div className="sidebar">
+                <ul className="sidebar-emotions-ul">
+                    <li>
+                        <button className='emotions-btn angry' onClick={() => onClickEmotionsBtn('angry')}>
+                            Angry
+                        </button>
+                    </li>
+                    <li>
+                        <button className='emotions-btn happy' onClick={() => onClickEmotionsBtn('happy')}>
+                            Happy
+                        </button>
+                    </li>
+                    <li>
+                        <button className='emotions-btn sad' onClick={() => onClickEmotionsBtn('sad')}>
+                            Sad
+                        </button>
+                    </li>
+                    <li>
+                        <button className='emotions-btn surprised' onClick={() => onClickEmotionsBtn('surprised')}>
+                            Surprised
+                        </button>
+                    </li>
+                    <li>
+                        <button className='emotions-btn disgusted' onClick={() => onClickEmotionsBtn('disgusted')}>
+                            Disgusted
+                        </button>
+                    </li>
+                    <li>
+                        <button className='emotions-btn fearful' onClick={() => onClickEmotionsBtn('fearful')}>
+                            Fearful
+                        </button>
+                    </li>
+                    <li>
+                        <button className='emotions-btn bad' onClick={() => onClickEmotionsBtn('bad')}>
+                            Bad
+                        </button>
+                    </li>
+                    <li>
+                        <button className='emotions-btn random' onClick={() => onClickEmotionsBtn('random')}>
+                            Random
+                        </button>
+                    </li>
+                    <li className='username-li-item'>
+                        {username}
+                    </li>
+                </ul>
+                
+            </div>
+        </>
+    )
+}
+
+export default Sidebar;
+
+
